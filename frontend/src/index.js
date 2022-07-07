8d7a853dd0948685cf4fa973afda1db280dcb02a
Suyash Vikram Singh <suyashvikramsingh2002@gmail.com>
2022-07-07 23:18:18 +0530

    Made PWA

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -5,14 +5,17 @@ import App from "./App";
 import { store } from "./app/store";
 import { Provider } from "react-redux";
 import { BrowserRouter } from "react-router-dom";
+import * as serviceWorkerRegistration from "./serviceWorkerRegistration";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  // <React.StrictMode>
-  <BrowserRouter>
-    <Provider store={store}>
-      <App />
-    </Provider>
-  </BrowserRouter>
-  //</React.StrictMode>
+  <React.StrictMode>
+    <BrowserRouter>
+      <Provider store={store}>
+        <App />
+      </Provider>
+    </BrowserRouter>
+  </React.StrictMode>
 );
+
+serviceWorkerRegistration.register();
